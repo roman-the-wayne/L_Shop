9bd6074ef41dc723328bf137ab7ad3202102678e
Walm1x <serikovegor2234@gmail.com>
2026-02-24 13:40:42 +0300

    добавление страниц

---
+++ client/src/app/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import BasketPage from "../pages/BasketPage";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/basket", element: <BasketPage /> },
+]);
